0d857b47b3013121223479de793624f905bde780
yourUsername <yourEmail@domain.com>
2019-12-27 23:16:13 -0500

    mabu

---
+++ src/main/java/com/example/demo/InputAverage.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class InputAverage {
+
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Input first number: ");            // this the hard print where it print the String
+            int num1 = in.nextInt();                            // this line is where the users add any number
+
+            System.out.print("Input second number: ");
+            int num2 = in.nextInt();
+
+            System.out.print("Input third number: ");
+            int num3 = in.nextInt();
+
+            System.out.print("Input fourth number: ");
+            int num4 = in.nextInt();
+
+            System.out.print("Enter fifth number: ");
+            int num5 = in.nextInt();
+
+
+            System.out.println("Average of five numbers is: " +
+                    (num1 + num2 + num3 + num4 + num5) / 5);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
